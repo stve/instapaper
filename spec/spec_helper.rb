7911a83137212b6692610f8df7d15b0d16c6a258
stve <steve.agalloco@gmail.com>
2015-03-03 00:06:00 -0500

    remove unused methods from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,37 +11,14 @@ require 'instapaper'
 require 'rspec'
 require 'webmock/rspec'
 
-def a_delete(path)
-  a_request(:delete, Instapaper::HTTP::Request::BASE_URL + path)
-end
-
-def a_get(path)
-  a_request(:get, Instapaper::HTTP::Request::BASE_URL + path)
-end
-
 def a_post(path)
   a_request(:post, Instapaper::HTTP::Request::BASE_URL + path)
-end
-
-def a_put(path)
-  a_request(:put, Instapaper::HTTP::Request::BASE_URL + path)
-end
-
-def stub_delete(path)
-  stub_request(:delete, Instapaper::HTTP::Request::BASE_URL + path)
-end
-
-def stub_get(path)
-  stub_request(:get, Instapaper::HTTP::Request::BASE_URL + path)
 end
 
 def stub_post(path)
   stub_request(:post, Instapaper::HTTP::Request::BASE_URL + path)
 end
 
-def stub_put(path)
-  stub_request(:put, Instapaper::HTTP::Request::BASE_URL + path)
-end
 
 def fixture_path
   File.expand_path('../fixtures', __FILE__)
