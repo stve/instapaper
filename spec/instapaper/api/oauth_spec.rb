ef7d4458d064095080d965cc0f175d5ac38f40fe
stve <steve.agalloco@gmail.com>
2015-10-06 23:24:54 -0400

    update oauth endpoints to 1.1 paths

--- spec/instapaper/api/oauth_spec.rb
+++ spec/instapaper/api/oauth_spec.rb
@@ -5,15 +5,15 @@ describe Instapaper::Client::OAuth do
 
   describe '#token' do
     before do
-      stub_post('/api/1/oauth/access_token').with(body: {x_auth_username: 'ohai', x_auth_password: 'p455w0rd', x_auth_mode: 'client_auth'})
+      stub_post('/api/1.1/oauth/access_token').with(body: {x_auth_username: 'ohai', x_auth_password: 'p455w0rd', x_auth_mode: 'client_auth'})
         .to_return(body: fixture('access_token.txt'), headers: {content_type: 'text/plain; charset=utf-8'})
-      stub_post('/api/1/oauth/access_token').with(body: {x_auth_username: 'inval1d', x_auth_password: 'cr3dentials', x_auth_mode: 'client_auth'})
+      stub_post('/api/1.1/oauth/access_token').with(body: {x_auth_username: 'inval1d', x_auth_password: 'cr3dentials', x_auth_mode: 'client_auth'})
         .to_return(body: fixture('invalid_credentials.txt'), headers: {content_type: 'text/plain; charset=utf-8'})
     end
 
     it 'gets the correct resource' do
       client.access_token('ohai', 'p455w0rd')
-      expect(a_post('/api/1/oauth/access_token'))
+      expect(a_post('/api/1.1/oauth/access_token'))
         .to have_been_made
     end
 
